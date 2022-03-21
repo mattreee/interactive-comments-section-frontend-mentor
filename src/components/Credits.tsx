4692b79368016096003e6969f61d439b59e45c6e
mattreee <fantinatomatheuss@gmail.com>
2022-03-21 18:32:08 -0400

    chore: added initial content and styling variables

---
+++ src/components/Credits.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-no-target-blank */
+const Credits = () => {
+	return (
+		<div className="attribution">
+			Challenge by{" "}
+			<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+				Frontend Mentor
+			</a>
+			. Coded by <a href="https://github.com/mattreee">mattreee</a>.
+		</div>
+	);
+};
+
+export default Credits;
