e21a24ed3f3abba5e97d973502354c62abbba5bc
mattreee <fantinatomatheuss@gmail.com>
2022-03-21 18:43:38 -0400

    chore: added data.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Comments from "./components/Comments";
 import Credits from "./components/Credits";
 import "./css/main.css";
 
 function App() {
 	return (
 		<div role="main">
+			<Comments />
 			<Credits />
 		</div>
 	);
