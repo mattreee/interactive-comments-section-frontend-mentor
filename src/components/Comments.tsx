e2ed9116c9cd6da592297427738fb7f50967bf61
mattreee <fantinatomatheuss@gmail.com>
2022-03-22 21:42:43 -0400

    chore: comments now load properly. some cors policy blocking problem still persists in some cases

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,5 +1,41 @@
+import { useState, useEffect } from "react";
+
+// interface commentsInterface {
+// 	comments: {
+// 		id: number;
+// 		content: string;
+// 		createdAt: string;
+// 		score: number;
+// 		user: {};
+// 		replies: [];
+// 	}[];
+// }
+
 const Comments = () => {
-	return <div>Comments</div>;
+	const [comments, setComments] = useState([]);
+
+	// Load comments once after the page loads
+	useEffect(() => {
+		fetch("https://mattreee-comment-api.herokuapp.com/comments", {
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+			},
+		})
+			.then((res) => res.json())
+			.then((data) => setComments(data));
+	}, []);
+
+	return (
+		<section>
+			{comments.map((elem: any) => (
+				<div key={String(Math.random())}>
+					<p>{elem.user.username}</p>
+					<p>{elem.content}</p>
+				</div>
+			))}
+		</section>
+	);
 };
 
 export default Comments;
